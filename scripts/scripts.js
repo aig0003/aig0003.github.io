c78bff7bfda259b190339ae240b209eed9e478d8
Alec <aig0003@auburn.edu>
2019-10-18 16:55:59 -0500

    Refactoring + CSS alterative via JS tests

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,7 +6,7 @@ function insert_navbar(nav_array) {
     for (i = 0; i < nav_array.length; i++) {
 
         document.getElementById('navlist').innerHTML += 
-        '<li><a href="">' + nav_array[i] + '</a><hr class="" align="left" height="1px" width="0px"></li>';
+        '<li><a href="skills.html">' + nav_array[i] + '</a><hr class="" align="left" height="1px" width="0px"></li>';
     }
     document.getElementById('navbar').innerHTML += '</ul>';
     /*for (i=0; i < nav_array.length;i++) {
@@ -16,6 +16,11 @@ function insert_navbar(nav_array) {
     
 }
 
+function change_backgroundcolor() {
+    alert("Hi");
+
+    document.body.style.backgroundImage = "linear-gradient(to right,rgba(255, 1, 234, 0.8) 0% ,rgba(0,0,0,1) 100%)";
+}
 
 function get_current_page() {
     var path = window.location.pathname;
